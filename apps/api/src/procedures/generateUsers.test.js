1d50e09b72b36235399c3ce8318779b62324fc8a
Amine Benaddi <aminebenaddi@gmail.com>
2023-08-12 23:32:58 +0100

    monorepo with trpc, fastify, microservices, nextjs, etc.

---
+++ apps/api/src/procedures/generateUsers.test.js
@@ -0,0 +1,48 @@
+import { describe, expect, it, vi } from "vitest";
+import { appRouter } from "../router";
+import * as clientModule from "../utils/client";
+
+vi.mock("../utils/client", () => ({
+  client: {
+    fakeData: {
+      mutate: vi.fn(),
+    },
+  },
+}));
+
+describe("generateUsers", () => {
+  it("calling client once", async () => {
+    const spy = vi.spyOn(clientModule.client.fakeData, "mutate");
+
+    const caller = appRouter.createCaller({});
+
+    await caller.generateUsers({
+      length: 10,
+      properties: [
+        "bio",
+        "firstName",
+        "lastName",
+        "sex",
+        "jobTitle",
+        "jobType",
+        "phoneNumber",
+      ],
+    });
+
+    expect(spy).toHaveBeenCalled(1);
+  });
+
+  it("calling client once", async () => {
+    const spy = vi.spyOn(clientModule.client.fakeData, "mutate");
+
+    const caller = appRouter.createCaller({});
+    try {
+      await caller.generateUsers({
+        length: 10,
+        properties: ["test"],
+      });
+    } catch (err) {}
+
+    expect(spy).not.toHaveBeenCalled(1);
+  });
+});
